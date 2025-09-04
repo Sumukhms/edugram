3befb1fa520fc74e08cadfbeaca6374fae5836b5
Sumukhms <sumukhms.25@gmail.com>
2025-09-04 22:05:28 +0530

    help me

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,82 +1,90 @@
-import React, { useState, useContext } from 'react';
-import '../css/Signin.css';
+import React, { useEffect, useState } from 'react';
+import '../css/Signup.css';
 import logo from '../img/logo.PNG';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { LoginContext } from '../context/LoginContext';
 
-export default function Signin() {
+export default function Signup() {
   const navigate = useNavigate();
-  const { setUserLogin } = useContext(LoginContext);
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
 
   const notifyA = (msg) => toast.error(msg);
   const notifyB = (msg) => toast.success(msg);
 
   const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
 
+  // API base URL from environment variable
   const API_BASE = process.env.REACT_APP_API_URL;
 
   const postData = () => {
+    // checking email
     if (!emailRegex.test(email)) {
       notifyA("Invalid email");
       return;
+    } else if (!passRegex.test(password)) {
+      notifyA("Password must be 8-15 characters long, with uppercase, lowercase, a number and a special characters for example #,?,!");
+      return;
     }
 
-    fetch(`${API_BASE}/signin`, {
+    // sending data to server
+    fetch(`${API_BASE}/signup`, {
       method: "post",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
+        name: name,
+        userName: userName,
         email: email,
         password: password
       })
     })
-      .then(res => res.json())
-      .then(data => {
-        if (data.error) {
-          notifyA(data.error);
-        } else {
-          notifyB("Signed In Successfully");
-          localStorage.setItem("jwt", data.token);
-          localStorage.setItem("user", JSON.stringify(data.user));
-          setUserLogin(true);
-          navigate("/");
-        }
-      })
-      .catch(err => {
-        console.error("Signin error:", err);
-        notifyA("Something went wrong");
-      });
+    .then(res => res.json())
+    .then(data => {
+      if (data.error) {
+        notifyA(data.error);
+      } else {
+        notifyB(data.message);
+        navigate("/signin");
+      }
+      console.log(data);
+    })
+    .catch(error => {
+      console.error("Signup error:", error);
+      notifyA("Something went wrong during signup.");
+    });
   };
 
   return (
-    <div className="signIn">
-      <div className='loginForm'>
-        <img className='signInLogo' src={logo} alt="" />
-        <div>
-          <input type="email" name='email' id='email'
-            value={email}
-            placeholder='Email'
-            onChange={(e) => setEmail(e.target.value)}
-          />
+    <div className='signUp'>
+      <div className="form-container">
+        <div className="form">
+
+          <img className='signUpLogo' src={logo} alt="" />
+          <p className='loginPara'>
+            Sign up to see educational content <br /> and share your knowledge. </p>
+          <div>
+            <input type="email" name='email' id='email' value={email} placeholder='Email' onChange={(e) => { setEmail(e.target.value); }} />
+          </div>
+          <div>
+            <input type="text" name='name' id='name' value={name} placeholder='Full Name' onChange={(e) => { setName(e.target.value); }} />
+          </div>
+          <div>
+            <input type="text" name='username' id='username' value={userName} placeholder='User Name' onChange={(e) => { setUserName(e.target.value); }} />
+          </div>
+          <div>
+            <input type="password" name='password' id='password' value={password} placeholder='Password ' onChange={(e) => { setPassword(e.target.value); }} />
+          </div>
+          <p className='loginPara' style={{ fontSize: "12px", margin: "3px" }}>By signing up , you agree to our Terms , <br /> privacy policy and cookies poilicy.</p>
+          <input type="submit" id='submit-btn' value='Sign Up' onClick={() => { postData(); }} />
         </div>
-        <div>
-          <input type="password" name='password' id='password'
-            value={password}
-            placeholder='Password'
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <input type="submit" id='login-btn' value="Sign In" onClick={postData} />
-        </div>
-        <div className="loginForm2">
-          Don't have an account ?
-          <Link to="/signup">
-            <span style={{ color: "blue", cursor: "pointer" }}>Sign Up</span>
+        <div className="form2">Already have an account ?
+          <Link to="/signin">
+            <span style={{ color: "blue", cursor: "pointer" }}> Sign In</span>
           </Link>
         </div>
       </div>
